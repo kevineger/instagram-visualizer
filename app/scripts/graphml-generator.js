419723f8e2a57aa653ff4d2c027d43c8ee02015f
kevineger <egerkevinjames@gmail.com>
2015-11-18 21:21:19 -0800

    Adds graphml functionality and download

--- app/scripts/graphml-generator.js
+++ app/scripts/graphml-generator.js
@@ -1,21 +1,63 @@
 var builder = require('xmlbuilder');
+var transformer = require('./transformers/d3Transformer');
+var fs = require('fs');
 
-var xml = builder.create('graphml')
-	.ele('graph', {'edgedefault': 'directed'})
-		// Node 1
-		.ele('node', {'id': '1'})
-			.ele('data', {'key': 'label'}, 'A')
-			.insertAfter('data', {'key': 'weight'}, '1.0')
-		.up().up()
-		// Node 2
-		.ele('node', {'id': '2'})
-			.ele('data', {'key': 'label'}, 'B')
-			.insertAfter('data', {'key': 'weight'}, '1.0')
-		// Edge
-		.up().up()
-		.ele('edge', {'source': '1', 'target': '2'})
-			.ele('data', {'key': 'label'}, 'A to B')
-		
-.end({ pretty: true});
+var xmlString;
 
-console.log(xml);
+// <key id="label" for="all" attr.name="label" attr.type="string"/>\
+// <key id="weight" for="node" attr.name="weight" attr.type="double"/>\
+
+var root = builder.create('graphml')
+.ele('key', {'id': 'label', 'for': 'node', 'attr.name': 'label', 'attr.type': 'string'})
+.up()
+.ele('key', {'id': 'weight', 'for': 'node', 'attr.name': 'weight', 'attr.type': 'double'})
+.up()
+.ele('graph', {'edgedefault': 'directed'});
+
+transformer.transform(function(data) {
+
+    // Array of nodes
+    var nodes = data.nodes;
+
+    // Array of edges
+    var edges = data.edges;
+
+    // Add all nodes
+    nodes.forEach(addNodeToGraphml);
+    // Add all edges
+    edges.forEach(addEdgeToGraphml);
+
+    xmlString = root.end({ pretty: true, indent: '  ', newline: '\n' });
+
+    // Write Graphml file to user's downloads folder
+    downloadsDir = process.env.HOME + "/Downloads/"
+    fs.writeFile(downloadsDir + 'instagram.graphml', xmlString, function(err) {
+    	if(err) {
+    		return console.log(err);
+    	}
+
+    	console.log("Graphml file saved as instagram.graphml to " + downloadsDir);
+    }); 
+
+})
+
+function addNodeToGraphml(node)
+{
+	console.log("Adding node to graphml" + node.id)
+	// Add node with id=InstagramID and name=username
+	root.ele('node', {'id': node.id})
+	.ele('data', {'key': 'label'}, node.name)			
+	.insertAfter('data', {'key': 'weight'}, '1.0')
+	// Reset pointer to graph root node
+	.up().up();
+}
+
+function addEdgeToGraphml(edge)
+{
+	console.log("Adding edge to graphml" + edge.to + " -> " + edge.from);
+	// Add edge with corresponding soure and target ids
+	root.ele('edge', {'source': edge.to, 'target': edge.from})
+	.ele('data', {'key': 'label'}, edge.to + ' to ' + edge.from)
+	// Reset pointer to graph root node
+	.up().up();
+}
