5d9265dddadf3bd66f66476e4ebdae87510202b7
distrill <brent@arthurkn.net>
2015-11-18 18:26:21 -0800

    vis.js and YONG mode

--- app/scripts/transformers/d3Transformer.js
+++ app/scripts/transformers/d3Transformer.js
@@ -2,7 +2,12 @@ var accessor = require('./../accessor');
 var _ = require('underscore');
 
 function getNode(userWithFollower){
-  return {name: userWithFollower.username, id: userWithFollower.id}
+  return {
+      name: userWithFollower.username,
+      id: userWithFollower.id,
+      shape: 'circularImage',
+      image: __dirname + '/../../assets/img/yong.jpg'
+  };
 }
 
 exports.transform = function(callback){
@@ -22,7 +27,11 @@ exports.transform = function(callback){
         //if followed is found in nodes, we create an edge between user.id and the followed node we found
         var followNode = _.findWhere(nodes, {id: followed});
         if(followNode){
-          edges.push({source: _.indexOf(nodes, _.findWhere(nodes, {id: user.id})), target: _.indexOf(nodes, followNode), value: 1})
+          edges.push({
+              source: _.indexOf(nodes, _.findWhere(nodes, {id: user.id})),
+              target: _.indexOf(nodes, followNode),
+              from: user.id,
+              to: followNode.id})
         }
       });
     });
