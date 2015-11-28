f39a1cb9e04469f9c3b1fb5fb82015737d71f57c
nkoriath <koriathsemail@gmail.com>
2015-11-27 22:15:59 -0800

    added some angular goodies

--- app/scripts/auth.js
+++ app/scripts/auth.js
@@ -2,10 +2,12 @@ const BrowserWindow = require('remote').require('browser-window');
 var vis = require('../scripts/instavis');
 var rp = require('request-promise');
 
-
 // Build the OAuth consent page URL
 var authWindow = new BrowserWindow({ width: 800, height: 600, show: true, 'node-integration': false, 'always-on-top': true});
 var authUrl = "https://api.instagram.com/oauth/authorize/?client_id=20020af689ea4d66a623067196c2987a&redirect_uri=http%3A%2F%2Flocalhost%3A8000&response_type=code";
+var userID;
+
+
 authWindow.loadUrl(authUrl);
 authWindow.show();
 
@@ -18,8 +20,8 @@ authWindow.webContents.on('did-get-redirect-request', function(event, oldUrl, ne
         //Convert er' to a string
         raw_code = newUrl.toString();
         console.log("FULL URL: "+ raw_code);
-        //Grab the fucking code here.
 
+        //Grab the fucking code here.
         raw_code = raw_code.substr(newUrl.indexOf('=')+1);
 
         //TODO: remove shitty console logging.
@@ -31,7 +33,6 @@ authWindow.webContents.on('did-get-redirect-request', function(event, oldUrl, ne
         //TODO: Something Useful Here
     }
 });
-
 
 function getToken(raw_code){
     console.log("GETTING THE MOTHERFUCKING TOKEN");
@@ -54,7 +55,9 @@ function getToken(raw_code){
         .then(function (parsedBody) {
             console.log("test: " + parsedBody.access_token);
             vis.vis(parsedBody.access_token);
+            userID = parsedBody.access_token;
             authWindow.close();
+            return userID;
         })
         .catch(function (err) {
             console.log(err);
