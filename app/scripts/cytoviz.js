da8ddf0ba5e2220d2ac39834254dc251302e8bb0
U-DESKTOP-A9A3B54\jakedavies <jgdavies24@gmail.com>
2015-12-03 17:03:46 -0800

    new layout buttons are life changing

--- app/scripts/cytoviz.js
+++ app/scripts/cytoviz.js
@@ -1,27 +1,26 @@
 var cytoscape = require('cytoscape');
 var transformer = require('./transformers/cytoviztransformer');
 var instwrapper = require('instawrapper');
-
-module.exports.vis = function(token){
+var graph;
+module.exports.vis = function(token) {
 	console.log("Hit Nodes Dump");
 	instwrapper.authorize(token);
 	transformer.transform(function(data) {
 		var options = {
-			name: 'cose',				
-			padding: 12,
-			animate: true,
-			gravity: 80,
-			animateThreshold: 250
+			name: 'cose'
 		};
-		var cy = cytoscape({
+		graph = cytoscape({
 			container: document.getElementById('graph'),
 		
 			elements: {
 				nodes: data.nodes,
 				edges: data.edges,
 			},
+			layout: options,
 			style: cytoscape.stylesheet().selector('node').css({'background-image':'http://cosc.ok.ubc.ca/__shared/assets/gao9404.jpg','background-fit':'cover'})
-		});	
-		cy.layout(options);
+		});
 	})
+}
+module.exports.changeLayout = function(layout) {
+	graph.layout({name: layout});
 }
