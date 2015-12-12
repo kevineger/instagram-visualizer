4b10945f0741345972572cc9c9d24fddba1b4a2e
kevineger <egerkevinjames@gmail.com>
2015-12-11 17:27:31 -0800

    Removes windows build from repo

--- app/Instagram-Visualizer-win32-x64/resources/app/InstaViz-win32-x64/resources/app/test.js
+++
@@ -1,5 +0,0 @@
-const transformer = require('./scripts/transformers/cytoviztransformer2.js');
-const accessor = require('./scripts/accessor');
-transformer.transform(function(data) {
-  console.log(data);
-});
